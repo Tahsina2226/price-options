dc40fa3a0c6843e0143e3dbe095939d80953f8b1
Tahsina2226 <tahsinatanvin274@gmail.com>
2024-10-29 21:31:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Daisynav from  './components/Daisynav'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <Daisynav></Daisynav>
+
+     
+      
+    </>
+  )
+}
+
+export default App
